ea705fd049685fa8be50b608dcead1ce69e17302
Golias <joao.victor@mobile2you.com.br>
2019-07-04 21:45:15 -0300

    added descriptive statisctis study

--- statistics_study/descriptive_statistics.py
+++ statistics_study/descriptive_statistics.py
@@ -1,9 +1,19 @@
-import statistics
+import pandas
+import numpy
 
 
 def main():
-    print(statistics.mean([1, 8, 4, 2, 3]))
-    print(statistics.median([1, 8, 4, 2, 3]))
+    iris = pandas.read_csv("iris.csv")
+    iris_petal_length = iris.iloc[:, 2]
+
+    numpy.random.seed(10)
+    sample = numpy.random.choice(iris_petal_length, 20, False)
+
+    print(numpy.mean(sample))
+    print(numpy.median(sample))
+    print(numpy.quantile(sample, [0, 0.25, 0.5, 0.75, 1]))
+    print(numpy.std(sample, ddof=1))# ao dividir, ele usa N-ddof. Como é uma amostra, temos que colocar como 1
+    return
 
 
 if __name__ == "__main__":
